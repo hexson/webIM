6faf7020677ecd06a42e3cd08988fd7a95b5d0ae
hexson <1477546184@qq.com>
2017-03-25 16:59:16 +0800

    update im js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,15 @@
 ;$(function(){
   $('.im-f-list').niceScroll({
     cursorcolor: '#d4d4d4',
-    cursorborder : '0px'
+    cursorborder : '0'
+  });
+  $('.im-f-list').on('click', '.im-f-item', function(){
+  	$('.im-f-item').removeClass('im-item-active');
+  	$(this).addClass('im-item-active');
+  });
+  $('textarea').focus(function(){
+  	$('.im-cvs-send').addClass('bfff');
+  }).blur(function(){
+  	$('.im-cvs-send').removeClass('bfff');
   });
 });
