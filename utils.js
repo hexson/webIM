ce5399cc89635fa15625a59979c53107199c262d
hadley <1477546184@qq.com>
2017-03-27 19:24:50 +0800

    update utils module

---
+++ utils.js
@@ -0,0 +1,33 @@
+var md5 = require('blueimp-md5');
+
+var utils = {
+  /*
+   * @Get now timestamp
+   */
+  getTime: function(){
+    return (Date.now() + '').substr(0, 10);
+  },
+  /*
+   * @Get an random strings
+   * @params len{Number}  strings length
+   * @params upperCase{Boolean}  strings have upper case
+   */
+  getRandomStr: function(len, upperCase){
+    var str = '0123456789abcdefghijklmnopqrstuvwxyz'
+    str += upperCase ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : '';
+    var l = str.length, i, outstr = '';
+    for (i = 0; i < len; i++){
+      outstr += str.charAt(Math.floor(Math.random() * l));
+    }
+    return outstr;
+  },
+  /*
+   * @String to md5
+   * @params str{String}  any
+   */
+  md5: function(str){
+    return md5(str);
+  }
+};
+
+module.exports = utils;
